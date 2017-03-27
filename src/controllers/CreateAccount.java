0604e9105a33c0f13c5889c2c604ba9db43ce3a4
JoshPTucker <jptucker2009@gmail.com>
2017-03-27 09:11:23 -0400

    worked on create account

--- src/controllers/CreateAccount.java
+++ src/controllers/CreateAccount.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.TnUser;
+import util.UserUtil;
+import dao.UserDao;
 
 /**
  * Servlet implementation class CreateAccount
